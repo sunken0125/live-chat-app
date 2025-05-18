2e7b651677846188244ef865df9f7ef58b7eb94b
sunken0125 <sankarshan125@gmail.com>
2025-05-18 18:05:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,14 +14,14 @@ const app = express();
 const server= http.createServer(app);
 const io= new Server(server,{
     cors:{
-        origin: 'http://localhost:3000',
+        origin: 'https://live-chat-app-lac.vercel.app/',
         methods: ['GET','POST'],
         credentials: true,
     }
 });
 
 app.use(cors(
-    { origin: 'http://localhost:3000' ,
+    { origin: 'https://live-chat-app-lac.vercel.app/' ,
         credentials:true,
     }
 ));
